50527f54d55dce5686c391039bc8e30d5e460fdf
Debasis Nayak <debasishnayak0739@gmail.com>
2024-02-05 12:26:01 +0530

    all done

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
-const apiKey = "AIzaSyBOjoZBhWKHB6ClZlSOJ37i7a8iqck1jEM";
-const baseUrl = "https://www.googleapis.com/youtube/v3";
+const API_KEY = "AIzaSyBOjoZBhWKHB6ClZlSOJ37i7a8iqck1jEM";
+const BASE_URL = "https://www.googleapis.com/youtube/v3";
 
 const searchBtn = document.querySelector('.search-btn');
 const searchInput = document.querySelector('.search-bar');
 const videoContainer = document.querySelector('.video-container');
 
-async function fetchDefaultVideos(searchQuery, maxItems){
-  let response = await fetch(`${baseUrl}/search?key=${apiKey}&q=${searchQuery}&maxResults=${maxItems}&part=snippet`);
-  let data = await response.json();
-  // console.log(data);
-  if (data.items) {
-    data.items.forEach(item => {
-      const videoElement = document.createElement('div');
-      videoElement.classList.add('video');
+window.addEventListener("DOMContentLoaded", () => {
+  // fetchDefaultVideos("icc", 20);
+  fetchDefaultVideos("", 20);
+});
 
-      videoElement.innerHTML = `<img src="${item.snippet.thumbnails.high.url}" class="thumbnail" alt="" />
-          <div class="content">
-            <img src="${item.channelObject[0].snippet.thumbnails.high.url}" class="channel-icon" alt="" />
-            <div class="info">
-              <h4 class="title">
-                ${item.snippet.title}
-              </h4>
-              <p class="channel-name">${item.snippet.channelTitle}</p>
-              <p class="channel-views">15K Views . 1 week ago</p>
-            </div>
-          </div>`;
-
-      videoContainer.appendChild(videoElement);
-    });
-  }
-
+async function fetchDefaultVideos(searchQuery, maxResults) {
+  const response = await fetch(
+    `${BASE_URL}/search?key=${API_KEY}&q=${searchQuery}&maxResults=${maxResults}&part=snippet`
+  );
+  const data = await response.json();
+  console.log(data.items);
+  displayVideos(data.items);
 }
 
-window.addEventListener('load', () => {
-  fetchDefaultVideos("icc", 20);
-});
+
+function displayVideos(videos){
+  videoContainer.innerHTML = '';
+  videos.forEach(video => {
+          let videoElement = document.createElement("div");
+          videoElement.classList.add('video');
+          videoElement.innerHTML = `
+          <a href="videoDetails.html?videoId=${video.id.videoId}">
+          <img src="${video.snippet.thumbnails.high.url}" class="thumbnail" alt="" />
+              <div class="content">
+                <img src="${video.snippet.thumbnails.default.url}" class="channel-icon" alt="" />
+                <div class="info">
+                  <h4 class="title">
+                    ${video.snippet.title}
+                  </h4>
+                  <p class="channel-name">${video.snippet.channelTitle}</p>
+                  <p class="channel-views">15K Views . 1 week ago</p>
+                </div>
+              </div>
+              </a>
+              `;
+          videoContainer.appendChild(videoElement);
+  });
+}
